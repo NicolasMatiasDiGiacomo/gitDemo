79793ef8cd202b6f587670ed2231ef7d98e1d91e
Nicolas Matias Di Giacomo <nicolasmatiasdigiacomo@nicolass-mac-mini.local>
2016-01-26 14:34:52 -0300

    add label "Hello World"

--- gitDemo/ViewController.m
+++ gitDemo/ViewController.m
@@ -16,6 +16,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    UILabel *helloLabel = [[UILabel alloc]initWithFrame:CGRectMake(10, 50, 200, 40)];
+    helloLabel.text = @"Hello World";
+    
+    [self.view addSubview:helloLabel];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
